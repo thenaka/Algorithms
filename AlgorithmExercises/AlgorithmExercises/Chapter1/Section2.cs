1124d9a13f8c1e0a71fb101fea50d694c4a6b400
Ryan Muranaka <rmuranaka@hotmail.com>
2022-03-11 09:01:45 -0700

    Started section 2 with random points method.

---
+++ AlgorithmExercises/AlgorithmExercises/Chapter1/Section2.cs
@@ -0,0 +1,32 @@
+﻿using AlgorithmExercises.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmExercises.Chapter1
+{
+	public class Section2
+	{
+		private static readonly Random _random = new();
+
+		/// <summary>
+		/// Get <paramref name="n"/> number of random points all within a unit square.
+		/// </summary>
+		/// <param name="n">Number of points to randomly generate.</param>
+		/// <returns><paramref name="n"/> number of random points within a unit square.</returns>
+		public static IEnumerable<Point> GetRandomPoints(int n)
+		{
+			List<Point> points = new();
+
+			for (int i = 0; i < n; i++)
+			{
+				double x = _random.NextDouble();
+				double y = _random.NextDouble();
+				points.Add(new Point(x, y));
+			}
+			return points;
+		}
+	}
+}
---
+++ AlgorithmExercises/AlgorithmExercisesTests/Chapter1/Section2Tests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmExercisesTests.Chapter1
+{
+	[TestFixture]
+	public class Section2Tests
+	{
+		[TestCase(10, TestName = "Exercise1_RandomPoints_GetClosestDistance(")]
+		public void Exercise1_RandomPoints_GetClosestDistance(int n)
+		{
+
+		}
+	}
+}
