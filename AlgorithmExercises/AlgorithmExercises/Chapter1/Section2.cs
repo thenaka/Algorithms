c39a75eba1c848d3562293810c7f7c12708016fa
Ryan Muranaka <rmuranaka@hotmail.com>
2022-04-15 09:28:58 -0600

    Completed exercises 4 through 8.

--- AlgorithmExercises/AlgorithmExercises/Chapter1/Section2.cs
+++ AlgorithmExercises/AlgorithmExercises/Chapter1/Section2.cs
@@ -53,5 +53,45 @@ namespace AlgorithmExercises.Chapter1
 				yield return new Interval2D(intervals.ElementAt(i), intervals.ElementAt(i + 1));
 			}
 		}
+
+		/// <summary>
+		/// Determines if <paramref name="s"/> and <paramref name="t"/> are circular rotations of each other.
+		/// </summary>
+		/// <param name="s">First string to check.</param>
+		/// <param name="t">Second string to check.</param>
+		/// <returns>True if <paramref name="s"/> and <paramref name="t"/> are circular rotations of each other.</returns>
+		public static bool CircularShift(string s, string t)
+		{
+			return $"{s}{s}".Contains(t);
+		}
+
+		/// <summary>
+		/// Reverses <paramref name="s"/>.
+		/// </summary>
+		/// <param name="s">String to reverse.</param>
+		/// <returns><paramref name="s"/> reversed.</returns>
+		public static string Reverse(string s)
+		{
+			int n = s.Length;
+			if (n <= 1) return s;
+
+			int halfN = n / 2;
+			string a = s.Substring(0, halfN);
+			string b = s.Substring(halfN, n - halfN);
+			return Reverse(b) + Reverse(a);
+		}
+
+		/// <summary>
+		/// Returns a collection of length <paramref name="n"/> filled with random integers.
+		/// </summary>
+		/// <param name="n">The number of integers to return.</param>
+		/// <returns>A collection of length <paramref name="n"/> filled with random integers.</returns>
+		public static IEnumerable<int> GetRandomInts(int n)
+		{
+			for (int i = 0; i < n; i++)
+			{
+				yield return _random.Next(int.MinValue, int.MaxValue);
+			}
+		}
 	}
 }
--- AlgorithmExercises/AlgorithmExercisesTests/Chapter1/Section2Tests.cs
+++ AlgorithmExercises/AlgorithmExercisesTests/Chapter1/Section2Tests.cs
@@ -41,9 +41,11 @@ namespace AlgorithmExercisesTests.Chapter1
 		[TestCase(30, TestName = "Exercise2_Interval1D_When30_DoIntersect")]
 		public void Exercise2_Interval1D_DoIntersect(int n)
 		{
+			// Arrange
 			var randomInterval1D = Section2.GetRandomInterval1D(n);
 			var combinations = new Combinations<Interval1D>(randomInterval1D, 2);
 
+			// Act
 			for (int i = 0; i < combinations.Count(); i++)
 			{
 				var firstInterval1D = combinations.ElementAt(i).ElementAt(0);
@@ -54,6 +56,7 @@ namespace AlgorithmExercisesTests.Chapter1
 					Console.WriteLine($"{firstInterval1D} intersects {secondInterval1D}");
 				}
 			}
+			// Assert
 			Assert.Pass();
 		}
 
@@ -62,9 +65,11 @@ namespace AlgorithmExercisesTests.Chapter1
 		[TestCase(30, TestName = "Exercise3_Interval2D_When30_DoIntersect")]
 		public void Exercise3_Interval2D_DoIntersect(int n)
 		{
+			// Arrange
 			var randomInterval2D = Section2.GetRandomInterval2D(n);
 			var combinations = new Combinations<Interval2D>(randomInterval2D, 2);
 
+			// Act
 			for (int i = 0; i < combinations.Count(); i++)
 			{
 				var firstInterval2D = combinations.ElementAt(i).ElementAt(0);
@@ -74,6 +79,7 @@ namespace AlgorithmExercisesTests.Chapter1
 					Console.WriteLine($"{firstInterval2D} intersects {secondInterval2D}");
 				}
 			}
+			// Assert
 			Assert.Pass();
 		}
 
@@ -82,9 +88,11 @@ namespace AlgorithmExercisesTests.Chapter1
 		[TestCase(30, TestName = "Exercise3_Interval2D_When30_Contains")]
 		public void Exercise3_Interval2D_Contains(int n)
 		{
+			// Arrange
 			var randomInterval2D = Section2.GetRandomInterval2D(n);
 			var combinations = new Combinations<Interval2D>(randomInterval2D, 2);
 
+			// Act
 			for (int i = 0; i < combinations.Count(); i++)
 			{
 				var firstInterval2D = combinations.ElementAt(i).ElementAt(0);
@@ -94,7 +102,84 @@ namespace AlgorithmExercisesTests.Chapter1
 					Console.WriteLine($"{firstInterval2D} contains {secondInterval2D}");
 				}
 			}
+			// Assert
 			Assert.Pass();
+		}
+
+		[Test]
+		public void Exercise4_ImmutableString()
+		{
+			// Arrange
+			string string1 = "hello";
+			string string2 = string1;
+
+			// Act
+			string1 = "world";
+
+			// Assert
+			Assert.That(string1, Is.EqualTo("world"));
+			Assert.That(string2, Is.EqualTo("hello"));
+		}
+
+		[Test]
+		public void Exercise5_ImmutableString()
+		{
+			// Arrange
+			string s = "Hello World";
+
+			// Act
+			_ = s.ToUpper();
+			_ = s.Substring(6, 5);
+
+			// Assert
+			Assert.That(s, Is.EqualTo("Hello World"));
+		}
+
+		[TestCase("abcdef", "cdefab", true, TestName = "Exercise6_CircularRotation_ReturnsExpected")]
+		[TestCase("abcdef", "fedcba", false, TestName = "Exercise6_CircularRotation_WhenFalse_ReturnsExpected")]
+		public void Exercise6_CircularRotation_ReturnsExpected(string s, string t, bool expected)
+		{
+			// Arrange
+
+			// Act
+			bool result = Section2.CircularShift(s, t);
+
+			// Assert
+			Assert.That(result, Is.EqualTo(expected));
+		}
+
+		[TestCase("test", TestName = "Exercise7_Reverse_ReturnsExpected")]
+		[TestCase("tests", TestName = "Exercise7_Reverse_When5Chars_ReturnsExpected")]
+		[TestCase("teststuff", TestName = "Exercise7_Reverse_When9Chars_ReturnsExpected")]
+		[TestCase("", TestName = "Exercise7_Reverse_WhenEmptyString_ReturnsExpected")]
+		public void Exercise7_Reverse_ReturnsExpected(string s)
+		{
+			// Arrange
+
+			// Act
+			string result = Section2.Reverse(s);
+
+			// Assert
+			Assert.That(result, Is.EqualTo(s.Reverse()));
+		}
+
+		[Test]
+		public void Exercise8_SwapArrays()
+		{
+			// Arrange
+			var a = Section2.GetRandomInts(1_000_000);
+			var originalA = a;
+			var b = Section2.GetRandomInts(1_000_000);
+			var originalB = b;
+
+			// Act
+			var t = a; 
+			a = b; 
+			b = t;
+
+			// Assert
+			Assert.That(a, Is.EqualTo(originalB));
+			Assert.That(b, Is.EqualTo(originalA));
 		}
 	}
 }
