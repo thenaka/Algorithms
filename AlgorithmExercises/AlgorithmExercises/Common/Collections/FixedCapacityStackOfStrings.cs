2e9674a5ca1d9ed206156e352de9387a8f637057
Ryan Muranaka <rmuranaka@hotmail.com>
2022-05-31 14:31:07 -0600

    Began implementation of Exercise 1.3.1

---
+++ AlgorithmExercises/AlgorithmExercises/Common/Collections/FixedCapacityStackOfStrings.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmExercises.Common.Collections
+{
+	public class FixedCapacityStackOfStrings
+	{
+		private string[] _items;
+		private int _currentSize;
+		
+		public FixedCapacityStackOfStrings(int capacity)
+		{
+			_items = new string[capacity];
+		}
+
+		public bool IsEmpty => _currentSize == 0;
+		public int Size => _currentSize;
+
+		public void Push(string item)
+		{
+			_items[_currentSize++] = item;
+		}
+
+		public string Pop()
+		{
+			return _items[--_currentSize];
+		}
+	}
+}
